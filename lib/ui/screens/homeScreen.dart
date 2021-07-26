f2903d317db239bf6ffbaccd89c2fe078c8a9766
wrteam-priyansh <wrteam.priyansh@gmail.com>
2021-07-26 10:08:59 +0530

    added tap animation

--- lib/ui/screens/homeScreen.dart
+++ lib/ui/screens/homeScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ui_ux/ui/screens/quizPlayAreaScreen.dart';
 import 'package:ui_ux/ui/screens/scrollDragAnimationScreen.dart';
 import 'package:ui_ux/ui/screens/selectWordAnimationScreen.dart';
+import 'package:ui_ux/ui/screens/tapAnimationScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<String> examples = ["Quiz Play Area", "Scroll Drag Animation", "Select Word Animation"];
+  final List<String> examples = ["Quiz Play Area", "Scroll Drag Animation", "Select Word Animation", "Tap Animation"];
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +27,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 Navigator.push(context, MaterialPageRoute(builder: (context) => ScrollDragAnimationScreen()));
               } else if (examples[index] == "Select Word Animation") {
                 Navigator.push(context, MaterialPageRoute(builder: (context) => SelectWordAnimationScreen()));
+              } else if (examples[index] == "Tap Animation") {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => TapAnimationScreen()));
               }
             },
             title: Text("${examples[index]}"),
--- lib/ui/screens/tapAnimationScreen.dart
+++ lib/ui/screens/tapAnimationScreen.dart
@@ -8,11 +8,45 @@ class TapAnimationScreen extends StatefulWidget {
 }
 
 class _TapAnimationScreenState extends State<TapAnimationScreen> with SingleTickerProviderStateMixin {
+  final Duration duration = Duration(milliseconds: 75);
+  final double endScale = 0.95;
+
+  late AnimationController animationController = AnimationController(vsync: this, duration: duration);
+  late Animation<double> animation = Tween<double>(begin: 1.0, end: endScale).animate(CurvedAnimation(parent: animationController, curve: Curves.easeInOut));
+
   @override
   Widget build(BuildContext context) {
+    Widget child = Center(
+      child: Container(
+        color: Theme.of(context).accentColor,
+        height: MediaQuery.of(context).size.height * (0.5),
+        width: MediaQuery.of(context).size.width,
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(title: Text("Tap Animation")),
-      body: Container(),
+      body: GestureDetector(
+        onTapCancel: () {
+          animationController.reverse();
+        },
+        onTapDown: (tapDownDetails) {
+          animationController.forward();
+        },
+        onTap: () {
+          animationController.reverse();
+        },
+        child: AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: animation.value,
+              child: child,
+            );
+          },
+          child: child,
+        ),
+      ),
     );
   }
 }
