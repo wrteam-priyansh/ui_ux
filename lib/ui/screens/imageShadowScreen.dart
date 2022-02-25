dc526340d0af407588e0c0032c7012509942d4b4
wrteam-priyansh <wrteam.priyansh@gmail.com>
2022-02-25 16:07:33 +0530

    completed image shadow

--- lib/ui/screens/imageShadowScreen.dart
+++ lib/ui/screens/imageShadowScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/shims/dart_ui_real.dart';
 
 class ImageShadowScreen extends StatelessWidget {
   const ImageShadowScreen({Key? key}) : super(key: key);
@@ -7,62 +8,70 @@ class ImageShadowScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Stack(
-        clipBehavior: Clip.none,
+      body: Column(
         children: [
-          Text("Helo"),
-          //
-          Padding(
-            padding: EdgeInsets.only(
-              top: 250,
-              right: MediaQuery.of(context).size.width * (0.15),
-              left: MediaQuery.of(context).size.width * (0.15),
-            ),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * (0.25),
-              width: MediaQuery.of(context).size.width,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(25),
-                child: Opacity(
-                  opacity: 0.95,
-                  child: Image.asset(
-                    "assets/images/woman_a.jpg",
-                    fit: BoxFit.fill,
+          Container(
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 270,
+                    right: MediaQuery.of(context).size.width * (0.125),
+                    left: MediaQuery.of(context).size.width * (0.125),
+                  ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * (0.225),
+                    width: MediaQuery.of(context).size.width,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(25),
+                      child: Opacity(
+                        opacity: 0.85,
+                        child: Image.asset(
+                          "assets/images/img8.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Positioned(
+                  top: 260,
+                  right: MediaQuery.of(context).size.width * (0.125),
+                  left: MediaQuery.of(context).size.width * (0.125),
+                  height: MediaQuery.of(context).size.height * (0.25),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 25.0, sigmaY: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(25.0)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: MediaQuery.of(context).size.width * (0.075),
+                  left: MediaQuery.of(context).size.width * (0.075),
+                  top: 250,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * (0.25),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage("assets/images/img8.png"))),
+                  ),
+                ),
+              ],
             ),
           ),
-
-          Positioned(
-            top: 250,
-            right: MediaQuery.of(context).size.width * (0.1),
-            left: MediaQuery.of(context).size.width * (0.1),
-            height: MediaQuery.of(context).size.height * (0.25),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.05),
-                    borderRadius: BorderRadius.circular(25.0)),
-              ),
-            ),
-          ),
-
-          Positioned(
-            right: MediaQuery.of(context).size.width * (0.075),
-            left: MediaQuery.of(context).size.width * (0.075),
-            top: 250,
-            child: Container(
-              height: MediaQuery.of(context).size.height * (0.25),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25.0),
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage("assets/images/woman_a.jpg"))),
-            ),
-          ),
+          Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "Hello",
+                style: TextStyle(fontSize: 30.0),
+              )),
         ],
       ),
     );
