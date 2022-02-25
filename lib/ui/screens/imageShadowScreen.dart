a3ce0b895d769233df6e52578e260d6f718e89a0
wrteam-priyansh <wrteam.priyansh@gmail.com>
2022-02-25 16:40:30 +0530

    cusomised imageShadow container

--- lib/ui/screens/imageShadowScreen.dart
+++ lib/ui/screens/imageShadowScreen.dart
@@ -10,61 +10,13 @@ class ImageShadowScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: [
-          Container(
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 270,
-                    right: MediaQuery.of(context).size.width * (0.125),
-                    left: MediaQuery.of(context).size.width * (0.125),
-                  ),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * (0.225),
-                    width: MediaQuery.of(context).size.width,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(25),
-                      child: Opacity(
-                        opacity: 0.85,
-                        child: Image.asset(
-                          "assets/images/img8.png",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 260,
-                  right: MediaQuery.of(context).size.width * (0.125),
-                  left: MediaQuery.of(context).size.width * (0.125),
-                  height: MediaQuery.of(context).size.height * (0.25),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 25.0, sigmaY: 25.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          borderRadius: BorderRadius.circular(25.0)),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  right: MediaQuery.of(context).size.width * (0.075),
-                  left: MediaQuery.of(context).size.width * (0.075),
-                  top: 250,
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * (0.25),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25.0),
-                        image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: AssetImage("assets/images/img8.png"))),
-                  ),
-                ),
-              ],
-            ),
+          ImageShadowContainer(
+            imageHeight: MediaQuery.of(context).size.height * (0.3),
+            imageWidth: MediaQuery.of(context).size.width,
+            imageUrl: "assets/images/dailyLesson-3.jpg",
+          ),
+          SizedBox(
+            height: 100.0,
           ),
           Align(
               alignment: Alignment.topCenter,
@@ -74,6 +26,111 @@ class ImageShadowScreen extends StatelessWidget {
               )),
         ],
       ),
+    );
+  }
+}
+
+class ImageShadowContainer extends StatelessWidget {
+  final double imageWidth;
+  final double imageHeight;
+  final String imageUrl;
+  final double? backgroundImageOpacity;
+  final double? shadowTopPadding;
+  final double? imageHorizontalMarginPercentage;
+  final double? topMargin;
+  final double? bottomMargin;
+  final double? borderRadius;
+  final double? blurEffect;
+  const ImageShadowContainer({
+    this.backgroundImageOpacity,
+    required this.imageHeight,
+    required this.imageUrl,
+    required this.imageWidth,
+    this.shadowTopPadding,
+    this.imageHorizontalMarginPercentage,
+    this.topMargin,
+    this.bottomMargin,
+    this.borderRadius,
+    this.blurEffect,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin:
+          EdgeInsets.only(top: topMargin ?? 25.0, bottom: bottomMargin ?? 0),
+      height: imageHeight,
+      width: imageWidth,
+      child: LayoutBuilder(builder: (context, boxConstraints) {
+        return Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: shadowTopPadding ?? (40),
+                right: boxConstraints.maxWidth *
+                    (imageHorizontalMarginPercentage == null
+                        ? 0.125
+                        : (imageHorizontalMarginPercentage! + 0.05)),
+                left: boxConstraints.maxWidth *
+                    (imageHorizontalMarginPercentage == null
+                        ? 0.125
+                        : (imageHorizontalMarginPercentage! + 0.05)),
+              ),
+              child: SizedBox(
+                height: boxConstraints.maxHeight,
+                width: boxConstraints.maxWidth,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(borderRadius ?? 25),
+                  child: Opacity(
+                    opacity: backgroundImageOpacity ?? 1.0,
+                    child: Image.asset(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: shadowTopPadding == null ? 30 : (shadowTopPadding! - 10),
+              right: boxConstraints.maxWidth *
+                  (imageHorizontalMarginPercentage == null
+                      ? 0.125
+                      : (imageHorizontalMarginPercentage! + 0.05)),
+              left: boxConstraints.maxWidth *
+                  (imageHorizontalMarginPercentage == null
+                      ? 0.125
+                      : (imageHorizontalMarginPercentage! + 0.05)),
+              height: boxConstraints.maxHeight,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                    sigmaX: blurEffect ?? 25.0, sigmaY: blurEffect ?? 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(borderRadius ?? 25)),
+                ),
+              ),
+            ),
+            Positioned(
+              left: boxConstraints.maxWidth *
+                  (imageHorizontalMarginPercentage ?? 0.075),
+              right: boxConstraints.maxWidth *
+                  (imageHorizontalMarginPercentage ?? 0.075),
+              child: Container(
+                height: boxConstraints.maxHeight,
+                width: boxConstraints.maxWidth,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(borderRadius ?? 25),
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: AssetImage(imageUrl))),
+              ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
